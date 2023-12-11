eded49de0b45d7b7b540f67d768435732e15ef41
Eugene Kovs <smartkovsik@gmail.com>
2023-12-11 16:03:48 +0300

    Initial Commit

---
+++ MyDayToday/MyDayTodayApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyDayTodayApp.swift
+//  MyDayToday
+//
+//  Created by Eugene Kovs on 11.12.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyDayTodayApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
